3317c6ed8120d213911ae6fce5169eba0a80c20c
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2022-09-17 20:25:36 +0700

    feat: add types declaration feature

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,28 @@
-import { defineConfig } from 'vitest/config';
+import ts from '@rollup/plugin-typescript';
+import { defineConfig, Plugin } from 'vite';
 
 export default defineConfig({
   test: {
     coverage: {
       provider: 'c8',
+    },
+  },
+  build: {
+    lib: {
+      entry: 'lib/index.ts',
+      formats: ['cjs', 'es'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      plugins: [
+        ts({
+          declaration: true,
+          declarationDir: 'dist',
+          emitDeclarationOnly: true,
+          baseUrl: '.',
+          rootDir: 'lib',
+        }) as Plugin,
+      ],
     },
   },
 });

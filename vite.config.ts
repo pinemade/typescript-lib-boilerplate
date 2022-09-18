db9351975ae1d151e484d7929dd783bc628867e0
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2022-09-18 20:36:39 +0700

    chore: rollback to rollup for bundling

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,12 @@
-import ts from '@rollup/plugin-typescript';
-import { defineConfig, Plugin } from 'vite';
+import { defineConfig } from 'vitest/config';
 
+/**
+ * config only for testing
+ */
 export default defineConfig({
   test: {
     coverage: {
       provider: 'c8',
-    },
-  },
-  build: {
-    lib: {
-      entry: 'lib/index.ts',
-      formats: ['cjs', 'es'],
-      fileName: 'index',
-    },
-    rollupOptions: {
-      plugins: [
-        ts({
-          declaration: true,
-          declarationDir: 'dist',
-          emitDeclarationOnly: true,
-          baseUrl: '.',
-          rootDir: 'lib',
-        }) as Plugin,
-      ],
     },
   },
 });

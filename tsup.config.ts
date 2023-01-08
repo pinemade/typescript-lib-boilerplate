616915fefb36b15411b65e88555df6413f472c52
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2023-01-08 19:43:36 +0700

    chore: update package, config prettier, eslint and tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,23 +2,23 @@ import type { Options } from 'tsup';
 import { defineConfig } from 'tsup';
 
 const commonOptions: Options = {
-	entry: ['lib/index.ts'],
-	clean: true,
-	dts: true,
-	splitting: false,
+    entry: ['lib/index.ts'],
+    clean: true,
+    dts: true,
+    splitting: false,
 };
 
 const buildOptions: Options[] = [
-	{
-		...commonOptions,
-		format: ['cjs'],
-		outDir: 'dist',
-	},
-	{
-		...commonOptions,
-		format: ['esm'],
-		outDir: 'dist/esm',
-	},
+    {
+        ...commonOptions,
+        format: ['cjs'],
+        outDir: 'dist',
+    },
+    {
+        ...commonOptions,
+        format: ['esm'],
+        outDir: 'dist/esm',
+    },
 ];
 
 export default defineConfig(buildOptions);
